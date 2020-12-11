125897a9dd2e96604a83df0b22e31add2991faaa
yangli <996426872@qq.com>
2020-12-11 23:09:28 +0800

    pytest homework1209

--- homework1209/test_calc.py
+++ homework1209/test_calc.py
@@ -1,19 +1,6 @@
 import pytest
 import yaml
-
-
-class Calculator:
-    def add(self, a, b):
-        return a+b
-
-    def sub(self, a, b):
-        return a-b
-
-    def mul(self, a, b):
-        return a*b
-
-    def div(self, a, b):
-        return a/b
+from com.calculator import Calculator
 
 
 class TestCalc:
